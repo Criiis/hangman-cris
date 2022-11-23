26170be3d672efb1ce4aecb8214c6350f6a3023a
Criiis <cristiano.luis@wundermanthompson.com>
2022-11-23 22:41:45 +0000

    create a letter word

--- src/components/HangmanWord.tsx
+++ src/components/HangmanWord.tsx
@@ -1,5 +1,26 @@
 export default function HangmanWord() {
+  const word = 'HangmanWord'
+  const wordGuest: string[] = []
+
   return (
-    <div>HangmanWord</div>
+    <div
+      style={{
+        display: 'flex',
+        gap: '.5rem',
+        fontSize: '2rem',
+        fontWeight: 'bold',
+        textTransform: 'uppercase',
+      }}
+    >
+      {word.split('').map((el, i) => (
+        <span style={{ borderBottom: '4px solid black' }} key={i}>
+          {!wordGuest?.includes(el.toUpperCase()) ? (
+            <span style={{ opacity: '0' }}>{i % 2 === 0 ? 'H' : 'A'}</span>
+          ) : (
+            <span>{wordGuest?.includes(el.toUpperCase()) ? el : ''}</span>
+          )}
+        </span>
+      ))}
+    </div>
   )
 }
