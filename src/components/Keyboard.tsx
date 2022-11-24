bf543cfc24711ef75d5395a6b1d189352510445c
Criiis <cristiano.luis@wundermanthompson.com>
2022-11-24 00:19:43 +0000

    keyboard style

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,5 +1,41 @@
+import styles from './Keyboard.module.css'
+const keys = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z',
+]
+
 export default function Keyboard() {
   return (
-    <div>Keyboard</div>
+    <div className={styles.keyboardContainer}>
+      {keys.map((key) => (
+        <button className={`${styles.btn} ${styles.active}`} key={key}>
+          {key}
+        </button>
+      ))}
+    </div>
   )
 }
