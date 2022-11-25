ce64cf16633685df18a482a6b810f5d1ec2ff576
Criiis <cristiano.luis@wundermanthompson.com>
2022-11-25 01:46:09 +0000

    change keyboard layout

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,38 +1,43 @@
+import { isConstructorDeclaration } from 'typescript'
 import styles from './Keyboard.module.css'
 const keys = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
+  'Q',
+  'W',
+  'E',
+  'R',
+  'T',
+  'Y',
+  'U',
+  'I',
+  'O',
+  'P',
+  'A',
+  'S',
+  'D',
+  'F',
+  'G',
+  'H',
+  'J',
+  'K',
+  'L',
+  'Z',
+  'X',
+  'C',
+  'V',
+  'B',
+  'N',
   'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z',
 ]
 
-export default function Keyboard() {
+export default function Keyboard({ letterClick }: any) {
   return (
     <div className={styles.keyboardContainer}>
       {keys.map((key) => (
-        <button className={`${styles.btn} ${styles.active}`} key={key}>
+        <button
+          onClick={() => letterClick(key)}
+          className={`${styles.btn} ${styles.active}`}
+          key={key}
+        >
           {key}
         </button>
       ))}
