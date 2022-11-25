ba6bc173d1821c3ff2d869c0979c2b2511b6db80
Criiis <cristiano.luis@wundermanthompson.com>
2022-11-25 01:15:31 +0000

    changing app tsx files

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
-import { useState } from "react";
-import HangmanDrawing from "./components/HangmanDrawing";
-import HangmanWord from "./components/HangmanWord";
-import Keyboard from "./components/Keyboard";
+import { useState } from 'react'
+import HangmanDrawing from './components/HangmanDrawing'
+import HangmanWord from './components/HangmanWord'
+import Keyboard from './components/Keyboard'
 import word from './wordList.json'
+import styles from './App.module.css'
 
 function App() {
-  const [wordToGuess, setWordToGuess] = useState(() => word[Math.floor(Math.random() * word.length)])
+  const [wordToGuess, setWordToGuess] = useState(
+    () => word[Math.floor(Math.random() * word.length)]
+  )
   const [guessLetters, setGuessLetters] = useState<string[]>([])
   console.log(wordToGuess)
 
   return (
-    <>
-      <div>Hello world!</div>
-      <HangmanDrawing/>
-      <HangmanWord/>
-      <Keyboard/>
-    </>
-  );
+    <div className={styles.app}>
+      <div>Lose Win</div>
+      <HangmanDrawing />
+      <HangmanWord wordToGuess={wordToGuess} />
+      <Keyboard />
+    </div>
+  )
 }
 
-export default App;
+export default App
