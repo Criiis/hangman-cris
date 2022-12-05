bb1ec1d374d4239e250e2818ea28554b2c0c1c73
Criiis <cristiano.luis@wundermanthompson.com>
2022-12-05 21:59:49 +0000

    make code for word simpler

--- src/components/HangmanWord.tsx
+++ src/components/HangmanWord.tsx
@@ -10,23 +10,14 @@ export default function HangmanWord({
   wordToGuess,
   letterGuessed,
 }: hangmanWordProps) {
-  const [lettersGuest, setLettersGuest] = useState(letterGuessed)
-
-  //update word
-  useEffect(() => {
-    setLettersGuest(letterGuessed)
-  }, [letterGuessed])
-
-  console.log(lettersGuest)
-
   return (
     <div className={styles.wordContainer}>
       {wordToGuess.split('').map((el, i) => (
         <span className={styles.wordBottom} key={i}>
-          {!lettersGuest?.includes(el.toUpperCase()) ? (
+          {!letterGuessed?.includes(el.toUpperCase()) ? (
             <span style={{ opacity: '0' }}>{i % 2 === 0 ? 'H' : 'A'}</span>
           ) : (
-            <span>{lettersGuest?.includes(el.toUpperCase()) ? el : ''}</span>
+            <span>{letterGuessed?.includes(el.toUpperCase()) ? el : ''}</span>
           )}
         </span>
       ))}
