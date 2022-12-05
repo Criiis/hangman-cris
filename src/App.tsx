f1e07d926896785ff191f70b692b8aa1a5d1928a
Criiis <cristiano.luis@wundermanthompson.com>
2022-12-05 22:00:47 +0000

    create a incorrectLetters

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useMemo } from 'react'
 import HangmanDrawing from './components/HangmanDrawing'
 import HangmanWord from './components/HangmanWord'
 import Keyboard from './components/Keyboard'
@@ -11,8 +11,17 @@ function App() {
   )
   const [guessLetters, setGuessLetters] = useState<string[]>([])
 
+  //check the incorrect letter
+  const [wordLetter] = useState(wordToGuess.toUpperCase().split(''))
+
+  //get only the incorrect letters
+  const incorrectLetters = useMemo(
+    () => guessLetters.filter((el) => !wordLetter.includes(el)),
+    [guessLetters, wordLetter]
+  )
+
+  //click event for screen keyboard
   const letterClick = (letter: string) => {
-    console.log('clicked')
     const letterUpper = letter.toUpperCase()
     if (guessLetters.includes(letterUpper)) return
     setGuessLetters((prev) => [...prev, letterUpper])
@@ -21,9 +30,9 @@ function App() {
   return (
     <div className={styles.app}>
       <div>Lose Win</div>
-      <HangmanDrawing />
+      <HangmanDrawing incorrectGuesses={incorrectLetters} />
       <HangmanWord wordToGuess={wordToGuess} letterGuessed={guessLetters} />
-      <Keyboard letterClick={letterClick} />
+      <Keyboard letterClick={letterClick} letterGuessed={guessLetters}/>
     </div>
   )
 }
--- src/components/HangmanDrawing.tsx
+++ src/components/HangmanDrawing.tsx
@@ -1,5 +1,15 @@
-export default function HangmanDrawing() {
+interface HangmanDrawingProps {
+  incorrectGuesses: string[]
+}
+
+export default function HangmanDrawing({
+  incorrectGuesses,
+}: HangmanDrawingProps) {
+  console.log('incorrectGuesses', incorrectGuesses)
   return (
-    <div>HangmanDrawing</div>
+    <div>
+      {incorrectGuesses.length}
+      <p>HangmanDrawing</p>
+    </div>
   )
 }
