fec981d876853f9d5e7a571a4bd2a37fd6ce46ad
Criiis <cristiano.luis@wundermanthompson.com>
2022-12-11 16:30:57 +0000

    create gallows style

--- src/components/HangmanDrawing.tsx
+++ src/components/HangmanDrawing.tsx
@@ -1,15 +1,37 @@
+import styles from './HangmanDrawing.module.css'
 interface HangmanDrawingProps {
   incorrectGuesses: string[]
+}
+
+const Gallows = () => {
+  return (
+    <>
+      <span className={styles.gallowsHangerEnd}></span>
+      <span className={styles.gallowsHanger}></span>
+      <span className={styles.gallowsPole}></span>
+      <span className={styles.gallowsBase}></span>
+    </>
+  )
+}
+const body = () => {
+  return <h1>body</h1>
+}
+
+const header = () => {
+  return <h1>head</h1>
 }
 
 export default function HangmanDrawing({
   incorrectGuesses,
 }: HangmanDrawingProps) {
+  const hangman = [body, header]
   console.log('incorrectGuesses', incorrectGuesses)
+  console.log(hangman[1]())
   return (
-    <div>
-      {incorrectGuesses.length}
-      <p>HangmanDrawing</p>
+    <div className={styles.gallowsContainer}>
+      {/* {incorrectGuesses.length} */}
+
+      <Gallows />
     </div>
   )
 }
